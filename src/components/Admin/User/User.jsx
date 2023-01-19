278e3ed5795a0b31d2ce9b198510b982e59a0f4b
Satis <satis@techxillence.com>
2023-01-19 15:33:54 +0545

    User table added

--- src/components/Admin/User/User.jsx
+++ src/components/Admin/User/User.jsx
@@ -1,24 +1,91 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import cursor from '../../../assets/images/learning.jpg'
-import Sidebar from '../Dashboard/Sidebar'
+import React from 'react';
+import { Button, Col, Container, Row, Table } from 'react-bootstrap';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+import cursor from '../../../assets/images/learning.jpg';
+import Sidebar from '../Dashboard/Sidebar';
 
 const User = () => {
+  const users = [
+    {
+      _id: 'lasjdasjlf',
+      name: 'Satis Kumar Chaudhary',
+      email: 'satis@gmail.com',
+      role: 'user',
+      subscription: {
+        status: 'active',
+      },
+    },
+  ];
+
+  const updateHandler = id => {
+    console.log('Updating the user with id', id);
+  };
+
+  const deleteHandler = id => {
+    console.log('Deleting the user with id ', id);
+  };
   return (
     <Container>
-        
-    <Row className='my-3' style={{cursor:`url(${cursor}),default`,height:'90vh'}}>
+      <Row
+        className="my-3"
+        style={{ cursor: `url(${cursor}),default`, height: '90vh' }}
+      >
         <Col md={10}>
-        User  dashboard
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>#id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Subscription</th>
+                <th isNumeric>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((item, index) => (
+                <tr key={item._id}>
+                  <td>#{item._id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.role}</td>
+                  <td>
+                    {item.subscription.status === 'active'
+                      ? 'active'
+                      : 'Not Active'}
+                  </td>
+                  <td isNumeric>
+                    <div>
+                      <Button
+                        variant="secondary"
+                        onClick={() => {
+                          updateHandler(item._id);
+                        }}
+                      >
+                        change role
+                      </Button>
+                      <Button
+                        variant="secondary"
+                        onClick={() => {
+                          deleteHandler(item._id);
+                        }}
+                        className="mx-2"
+                      >
+                        <RiDeleteBin7Fill />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </Col>
         <Col md={2}>
-            
-            <Sidebar/>
+          <Sidebar />
         </Col>
-    </Row>
+      </Row>
     </Container>
+  );
+};
 
-  )
-}
-
-export default User
+export default User;
